89f940db5c872c3e255e4e734ad7d07fa87dc3bb
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2024-09-10 18:30:04 +0200

    Use sudo for feelpp install

--- feelpp-install.sh
+++ feelpp-install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set +x
 
+echo $(whoami)
+
 # install feelpp stuff
 if [ ! -f /etc/apt/sources.list.d/feelpp.list ]; then
     echo "Installing feelpp packages"
     DIST=$(lsb_release -cs)
-    apt-get install wget gpg
-    wget -qO - http://apt.feelpp.org/apt.gpg | apt-key add -
-    echo "deb http://apt.feelpp.org/debian/$DIST $DIST latest" | tee -a /etc/apt/sources.list.d/feelpp.list
+    sudo apt-get install wget gpg
+    sudo wget -qO - http://apt.feelpp.org/apt.gpg | sudo apt-key add -
+    echo "deb http://apt.feelpp.org/debian/$DIST $DIST latest" | sudo tee -a /etc/apt/sources.list.d/feelpp.list
     rm -f feelpp.gpg
-    apt -qq update
-    apt-get -y install  --no-install-recommends \
+    sudo apt -qq update
+    sudo apt-get -y install  --no-install-recommends \
                python3-feelpp-toolboxes-coefficientformpdes \
        	       python3-feelpp-toolboxes-thermoelectric python3-feelpp-toolboxes-electric python3-feelpp-toolboxes-heat \
       	       python3-feelpp-toolboxes-fluid python3-feelpp-toolboxes-solid \
